74b3a2b975ae1839208dfba9a5854f448535357d
nadia-amz <nadiaamouz@gmail.com>
2023-10-03 17:11:02 +0200

    commit11

---
+++ src/Unit Test/Reservation.test.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Reservation from "../components/Reservation";
+
+// Mock the useNavigate hook
+jest.mock("react-router-dom", () => ({
+  useNavigate: jest.fn(),
+}));
+
+describe("Reservation Component", () => {
+  it("renders the component without errors", () => {
+    const { getByText, getByLabelText } = render(<Reservation />);
+    
+    // Check that the component renders successfully
+    expect(getByText("Reserve a table")).toBeInTheDocument();
+
+    // Example: Check for an element by its label text
+    expect(getByLabelText("Seating options :")).toBeInTheDocument();
+  });
+
+  it("handles changes in seating options", () => {
+    const { getByLabelText } = render(<Reservation />);
+
+    // Check that the standard checkbox can be checked and unchecked
+    const standardCheckbox = getByLabelText("Standard");
+    fireEvent.click(standardCheckbox);
+    expect(standardCheckbox).toBeChecked();
+    fireEvent.click(standardCheckbox);
+    expect(standardCheckbox).not.toBeChecked();
+
+    // Similar checks for the outdoor checkbox
+    const outdoorCheckbox = getByLabelText("Outdoor");
+    fireEvent.click(outdoorCheckbox);
+    expect(outdoorCheckbox).toBeChecked();
+    fireEvent.click(outdoorCheckbox);
+    expect(outdoorCheckbox).not.toBeChecked();
+  });
+
+  // You can write similar tests for other form elements and interactions.
+
+  it("handles form submission with incomplete data", () => {
+    const { getByText } = render(<Reservation />);
+    const submitButton = getByText("Submit");
+
+    // Click the submit button without checking any checkboxes
+    fireEvent.click(submitButton);
+
+    // Check that an error message is displayed
+    expect(getByText("Oops! The form is uncompleted")).toBeInTheDocument();
+  });
+
+  it("handles form submission with complete data", () => {
+    const { getByText, getByLabelText } = render(<Reservation />);
+    const submitButton = getByText("Submit");
+
+    // Check the standard checkbox
+    const standardCheckbox = getByLabelText("Standard");
+    fireEvent.click(standardCheckbox);
+
+    // Click the submit button
+    fireEvent.click(submitButton);
+
+    // Check that the component navigates to the confirmation page
+    // You need to mock the useNavigate hook for this to work
+    expect(submitButton).toHaveProperty("navigate");
+  });
+});
