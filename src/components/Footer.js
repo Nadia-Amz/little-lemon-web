3940258e3e4eb75c0f3286f6daebf962242589ad
nadia-amz <nadiaamouz@gmail.com>
2024-05-28 15:47:31 +0200

    commited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,75 +1,87 @@
 import React from "react";
 import logoFooter from "../images/logoFooter.jpg";
-import '../components/css/Style.css';
+import "../components/css/Style.css";
 
 const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="logo">
+        <img className="logo2" src={logoFooter} alt=""></img>
+      </div>
 
-    return (
-        <footer className="footer">
+      <section className="about">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+        </ul>
 
-            <div className="logo">
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
 
-                <img className="logo2" src={logoFooter} alt=""></img>
+        <ul>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+        </ul>
 
-            </div>
+        <ul>
+          <li>
+            <a href="#reservation">Reservation</a>
+          </li>
+        </ul>
 
-            <section className="about">
+        <ul>
+          <li>
+            <a href="#orderonline">Order Online</a>
+          </li>
+        </ul>
 
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                </ul>
+        <ul>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+        </ul>
+      </section>
 
-                <ul>
-                    <li><a href="#about">About</a></li>
-                </ul>
+      <section className="about">
+        <ul>
+          <li>
+            <a href="#address">Address</a>
+          </li>
+        </ul>
 
-                <ul>
-                    <li><a href="#menu">Menu</a></li>
-                </ul>
+        <ul>
+          <li>
+            <a href="#phonenumber">Phone Number</a>
+          </li>
+        </ul>
 
-                <ul>
-                    <li><a href="#reservation">Reservation</a></li>
-                </ul>
+        <ul>
+          <li>
+            <a href="#email">Email</a>
+          </li>
+        </ul>
+      </section>
 
-                <ul>
-                    <li><a href="#orderonline">Order Online</a></li>
-                </ul>
-
-                <ul>
-                    <li><a href="#login">Login</a></li>
-                </ul>
-
-            </section>
-
-            <section className="about">
-
-                <ul>
-                    <li><a href="#address">Address</a></li>
-                </ul>
-
-                <ul>
-                    <li><a href="#phonenumber">Phone Number</a></li>
-                </ul>
-
-                <ul>
-                    <li><a href="#email">Email</a></li>
-                </ul>
-
-
-            </section>
-
-            <section className="about">
-
-                <ul>
-                    <li> <a href="#facebook">Facebook</a> </li>
-                </ul>
-                <ul>
-                    <li> <a href="#instagram">Instagram</a> </li>
-                </ul>
-
-            </section>
-        </footer>
-
-    );
+      <section className="about">
+        <ul>
+          <li>
+            {" "}
+            <a href="#facebook">Facebook</a>{" "}
+          </li>
+        </ul>
+        <ul>
+          <li>
+            {" "}
+            <a href="#instagram">Instagram</a>{" "}
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
 };
 export default Footer;
